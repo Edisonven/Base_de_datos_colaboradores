31a8846871a8f00fdb82aaae7f914e9ccb1b7ffc
Edison <aalevenegass@gmail.com>
2023-12-23 12:41:42 -0300

    Actualizando componente app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,81 @@
-import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import BaseColaboradores from "./Colaboradores";
 import Listado from "./components/Listado";
 import Formulario from "./components/Formulario";
+import FormAlert from "./components/Alert";
 import Buscador from "./components/Buscador";
-import BaseColaboradores from "./Colaboradores";
-import Alerta from "./components/Alert";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [errores, setErrores] = useState("");
-  const [registrado, setRegistrado] = useState("");
+  const [listaColaboradores, setListaColaboradores] =
+    useState(BaseColaboradores);
+  const [errores, setErrores] = useState(false);
+  const [exito, setExito] = useState("");
+  const [searchPerson, setSearchPerson] = useState("");
+  const [formValue, setFormValue] = useState({
+    id: "",
+    nombre: "",
+    correo: "",
+    edad: "",
+    cargo: "",
+    telefono: "",
+  });
+  const nuevaBaseDatos = [...listaColaboradores, formValue];
+  const buscarColaborador = (event) => {
+    setSearchPerson(event.target.value);
+    if (event.target.value === "") {
+      setListaColaboradores(listaColaboradores);
+    } else {
+      const results = [...nuevaBaseDatos].filter((colaborador) =>
+        colaborador.nombre
+          .toLocaleLowerCase()
+          .includes(event.target.value.toLocaleLowerCase())
+      );
+      console.log(results);
+      setListaColaboradores(results);
+    }
+  };
 
   return (
     <div className="container">
       <div>
-        <h1 className="titulo">Agregar Colaborador</h1>
-        <Buscador />
-        <Listado listadoUsuarios={BaseColaboradores} />
+        <h1>Lista de colaboradores</h1>
+        <Buscador
+          searchPerson={searchPerson}
+          buscarColaborador={buscarColaborador}
+        ></Buscador>
+        <Listado
+          listaColaboradores={listaColaboradores}
+          setListaColaboradores={setListaColaboradores}
+          setSearchPerson={setSearchPerson}
+        ></Listado>
       </div>
-      <div className="container__formulario">
+      <div>
         <Formulario
+          listaColaboradores={listaColaboradores}
+          setListaColaboradores={setListaColaboradores}
           errores={errores}
-          registrado={registrado}
+          setExito={setExito}
           setErrores={setErrores}
-          setRegistrado={setRegistrado}
-        />
-        {errores ? <Alerta color="danger" mensajeAlerta={errores} /> : null}
-        {registrado ? (
-          <Alerta color="success" mensajeAlerta={registrado} />
+          exito={exito}
+          nuevaBaseDatos={nuevaBaseDatos}
+          formValue={formValue}
+          setFormValue={setFormValue}
+        ></Formulario>
+        {errores ? (
+          <FormAlert
+            variant="danger"
+            errores={errores}
+            setErrores={setErrores}
+          ></FormAlert>
+        ) : null}
+        {exito ? (
+          <FormAlert
+            variant="success"
+            exito={exito}
+            setExito={setExito}
+          ></FormAlert>
         ) : null}
       </div>
     </div>
