3ef3505a3715e81486d7aedc2af17174f19d02e9
bladimirlpz <bladimir.lopez.p@gmail.com>
2023-12-23 10:10:39 -0300

    Añadiendo los componentes a "APP"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Listado from './components/Listado'
+import Formulario from './components/Formulario'
+import Buscador from './components/Buscador';
+import BaseColaboradores from './Colaboradores';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [error, setError] = useState("");
+  const [registrado, setRegistrado] = useState("");
+  
   return (
     <>
-      
+      <h1 className='titulo'>Agregar Colaborador</h1>
+      <Formulario
+      error={error} 
+      registrado={registrado}
+      setError={setError}
+      setRegistrado={setRegistrado}/>
+      <Listado listadoUsuarios={BaseColaboradores}/>
+      <Buscador/>
     </>
   )
 }
