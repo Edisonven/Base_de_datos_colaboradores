3ed80d85dece96729e9123b365353afcd5b0480c
Edison <aalevenegass@gmail.com>
2023-12-23 12:43:30 -0300

    Cambiando componente buscador

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,54 +1,15 @@
-import { useState } from "react";
-import BaseColaboradores from "../Colaboradores";
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Table from 'react-bootstrap/Table';
-import './Buscador.css'
-
-function Buscador() {
-    const [filtrarNombre, setFiltrarNombre] = useState("");
-    const [listadoUsuario, setListaUsuario] = useState(BaseColaboradores)
-
-    // Constante que obtiene el nombre del input buscador
-    const filtrar = (e) => {
-        setFiltrarNombre(e.target.value)
-    }
-    // Variable que guarda un array con las propiedades del nombre del input buscador
-    const filtroId = listadoUsuario.filter((usu) => usu.nombre === filtrarNombre)
-       
+import Form from "react-bootstrap/Form";
+const Buscador = ({ searchPerson, buscarColaborador }) => {
   return (
     <>
-    <section className="buscador">
-    <InputGroup className="mb-3" onChange={filtrar}>
-      <InputGroup.Text >Busqueda</InputGroup.Text>
-      <Form.Control 
-      aria-label="First name"
-       />
-    </InputGroup>
-    <Table striped bordered hover variant="dark" size="lg"> 
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Edad</th>
-            <th>Cargo</th>
-            <th>Telefono</th>
-          </tr>
-        </thead>
-        <tbody id="tabla-buscador">
-            {filtroId.map(usu => 
-            <tr key={usu.id}>
-            <td>{usu.nombre}</td>
-            <td>{usu.correo}</td>
-            <td>{usu.edad}</td>
-            <td>{usu.cargo}</td>
-            <td>{usu.telefono}</td>
-            </tr>)}
-        </tbody>
-      </Table>
-      </section>
+      <Form.Control
+        className="buscador"
+        placeholder="Buscar colaborador"
+        value={searchPerson}
+        onChange={buscarColaborador}
+      />
     </>
   );
-}
+};
 
 export default Buscador;
