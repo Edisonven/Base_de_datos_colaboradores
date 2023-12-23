b61e308d9f60c4e29875dc39563d06c52fb623c4
Edison <aalevenegass@gmail.com>
2023-12-23 19:48:43 -0300

    Corrigiendo bug de filtrado

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -3,15 +3,15 @@ import Form from "react-bootstrap/Form";
 // componente que realiza la búsqueda que se ingresa en el input buscador
 const Buscador = ({ setFiltro, listaColaboradores }) => {
   const buscarColaborador = (event) => {
-    if (event.target.value === "") {
-      setFiltro(listaColaboradores);
-    } else {
+    if (event.target.value !== "") {
       const results = [...listaColaboradores].filter((colaborador) =>
         Object.values(colaborador).some((valor) =>
           String(valor).toLowerCase().includes(event.target.value)
         )
       );
       setFiltro(results);
+    } else {
+      setFiltro("");
     }
   };
   return (
